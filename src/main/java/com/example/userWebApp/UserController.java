7bb480e51e03bda6b0efe72eee10d2356c984598
ram726 <ramranjeet726@gmail.com>
2023-02-24 23:51:06 +0530

    sorting implemented

--- src/main/java/com/example/userWebApp/UserController.java
+++ src/main/java/com/example/userWebApp/UserController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -22,11 +23,13 @@ public class UserController {
 
     @GetMapping("/")
     public String home(Model model){
-        return listByPage(model,1);
+        return listByPage(model,1,"name","asc");
     }
     @GetMapping("/page/{pageNumber}")
-    public String listByPage(Model model,@PathVariable("pageNumber") Integer currentPage){
-        Page<User> page = userService.getAllUser(currentPage);
+    public String listByPage(Model model, @PathVariable("pageNumber") Integer currentPage,
+                             @RequestParam("sortField") String sortField,
+                             @RequestParam("sortDir") String sortDir){
+        Page<User> page = userService.getAllUser(currentPage,sortField,sortDir);
         long totalItems= page.getTotalElements();
         int totalPages = page.getTotalPages();
         List<User> list = page.getContent();
@@ -34,6 +37,9 @@ public class UserController {
         model.addAttribute("totalItems",totalItems);
         model.addAttribute("totalPages",totalPages);
         model.addAttribute("any",list);
+        model.addAttribute("sortField",sortField);
+        model.addAttribute("sortDir",sortDir);
+        model.addAttribute("checkDir",sortDir.equals("asc")?"desc":"asc");
         return "home";
     }
 
--- src/main/java/com/example/userWebApp/UserService.java
+++ src/main/java/com/example/userWebApp/UserService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 
@@ -12,8 +13,13 @@ public class UserService {
     @Autowired
     private UserDao userDao;
 
-    public Page<User> getAllUser(int pageNumber){
-        Pageable pageable = PageRequest.of(pageNumber-1,5);
+    public Page<User> getAllUser(int pageNumber, String sortField, String sortDir){
+//        Sort sort = Sort.by("name").ascending();
+//        sort = sort.equals("asc") ? sort.descending():sort.ascending();
+
+        Sort sort = sortDir.equalsIgnoreCase(Sort.Direction.ASC.name())
+                ?Sort.by(sortField).ascending():Sort.by(sortField).descending();
+        Pageable pageable = PageRequest.of(pageNumber-1,5,sort);
         return userDao.findAll(pageable);
     }
 }
